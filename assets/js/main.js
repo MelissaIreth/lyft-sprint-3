cba52396d3e5fe85fae3c22f5437cec0de5af5f3
Melissa <melissa.pacheco.martinez@gmail.com>
2017-05-23 20:41:58 -0400

    inicio tablero

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,3 +13,37 @@ function validarTelefono(){
 }
 
 validarTelefono();
+
+//Tablero y auto
+
+var start = document.getElementsByClassName("jugar")[0]
+start.addEventListener("click")
+
+var tablero = [
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0],
+[0,0,0,0,0,0,0,0,0,0]
+]
+
+var cooX = document.getElementsByClassName("x")[0];
+var cooY = document.getElementsByClassName("y")[0];
+var divT = document.getElementById("tablero");
+
+var linea;
+var cuadrito;
+
+for(var i=0; i<tablero.length; i++){
+	linea = document.createElement("div");
+	linea.classList.add("linea")
+
+	for(var j=0; j<tablero[i].length; j++){
+		cuadrito.innerHTML = tablero[i][j];
+		cuadrito.classList.add("cuadrito")
+		linea.appendChild(cuadrito);
+	}
+	divT.appendChild(linea)
+}
+
